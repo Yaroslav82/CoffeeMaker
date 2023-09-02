72c6b046aafd065124b85e9a4ad675eb66013c95
Yaroslav82 <yargay230607@gmail.com>
2023-09-02 15:29:46 +0300

    Add printMenu method

--- src/app/CoffeeMachine.java
+++ src/app/CoffeeMachine.java
@@ -1,4 +1,12 @@
 package app;
 
 public class CoffeeMachine {
+
+    private void printMenu() {
+        System.out.println("""
+                Choose a drink:
+                      1 - Espresso
+                      2 - Cappuccino
+                      3 - Latte""");
+    }
 }
