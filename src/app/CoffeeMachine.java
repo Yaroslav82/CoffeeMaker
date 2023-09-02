8902b7207743023d8d5c5ae1f2df5c7cf9490fd0
Yaroslav82 <yargay230607@gmail.com>
2023-09-02 15:30:24 +0300

    Add getCoffeeById method

--- src/app/CoffeeMachine.java
+++ src/app/CoffeeMachine.java
@@ -2,6 +2,18 @@ package app;
 
 public class CoffeeMachine {
 
+    private Coffee getCoffeeById(Integer id) {
+        Coffee coffee = null;
+        if (id.equals(1)) {
+            coffee = new Espresso();
+        } else if (id.equals(2)) {
+            coffee = new Cappuccino();
+        } else if (id.equals(3)) {
+            coffee = new Latte();
+        }
+        return coffee;
+    }
+
     private void printMenu() {
         System.out.println("""
                 Choose a drink:
